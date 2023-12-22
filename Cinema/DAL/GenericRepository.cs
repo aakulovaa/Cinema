f9924b1aded22d2ac8449bfa64d97781962f403f
aakulovaa <aakulovaa03@gmail.com>
2023-12-22 16:40:46 +0300

    crud class + controllers

--- Cinema/DAL/GenericRepository.cs
+++ Cinema/DAL/GenericRepository.cs
@@ -10,7 +10,7 @@ namespace Cinema.DAL
         {
             IQueryable<T> GetAll();
             T GetById(object id);
-            void Insert(T entity);
+            void Insert(T entity); 
             void Update(T entity);
             void Delete(object id);
             void Save();
